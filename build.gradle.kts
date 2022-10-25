7f3d0bda03dc87b96f2f92637a2c3672305a2ae4
mbruty <mbruty@users.noreply.github.com>
2022-10-25 14:23:56 +0000

    [ci skip] Bump version number

--- build.gradle.kts
+++ build.gradle.kts
@@ -62,7 +62,7 @@ apply<ReleasePlugin>()
 val exposedVersion = "0.38.2"
 
 group = "net.bruty"
-version = "0.8.10"
+version = "0.9.11"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
